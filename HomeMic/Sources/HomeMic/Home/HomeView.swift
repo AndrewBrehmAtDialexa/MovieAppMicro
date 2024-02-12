f061858b7a04fbbeebf45f43fecb3f81ed43b203
Lee McCormick <lee.mccormick@dialexa.com>
2024-02-12 14:25:36 -0600

    commonUI : Got new packag, colors and  buttons
    - Created new package called SharedUIs
    - Added Color Extension
    - Create Buttons directory and add ButtonStyle and use it in Home Mircro App

--- HomeMic/Sources/HomeMic/Home/HomeView.swift
+++ HomeMic/Sources/HomeMic/Home/HomeView.swift
@@ -1,9 +1,9 @@
 import Navigation
 import SwiftUI
+import SharedUIs
 
 public struct HomeView: View {
     @ObservedObject var viewModel = HomeViewModel()
-
     public init() {}
 
     public var body: some View {
@@ -11,10 +11,24 @@ public struct HomeView: View {
             Image(systemName: "house")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("HOME VIEW!!!")
+                .foregroundColor(.primaryDark)
+            
             Button("See a Cat!") {
                 viewModel.seeCatButtonTapped()
             }
+            .buttonStyle(GreenButton(isEnabled: true))
+            
+            Button("See a Cat! is Enabled is false ") {
+                viewModel.seeCatButtonTapped()
+            }
+            .buttonStyle(GreenButton(isEnabled: false))
+            
+            Button("See a Cat! with Light Green Color ") {
+                viewModel.seeCatButtonTapped()
+            }
+            .buttonStyle(LightGreenButton())
         }
         .padding()
     }
