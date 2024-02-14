6a412ae5543adf3eb9eab419799daa48f2633f15
Lee McCormick <lee.mccormick@dialexa.com>
2024-02-14 12:14:04 -0600

    Ready to PR, Cleaned up homeView and movie Card
    - Cleaned up home view by using commom UI and added mock data to make it prettier
    - Cleaned up movie Card view to load image from movie.url

--- HomeMic/Sources/HomeMic/Home/HomeView.swift
+++ HomeMic/Sources/HomeMic/Home/HomeView.swift
@@ -4,26 +4,80 @@ import CommonUI
 
 public struct HomeView: View {
     @ObservedObject var viewModel = HomeViewModel()
-
+    
     public init() {}
 
     var didAppear: ((Self) -> Void)?
 
     public var body: some View {
         VStack {
-            Image(systemName: "house")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("HOME VIEW!!!")
-                .largeTitleTextStyle()
-                .padding(.top, 10)
-            Button("See a Cat!") {
-                viewModel.seeCatButtonTapped()
+            HStack {
+                Image(systemName: "house")
+                    .imageScale(.large)
+                    .foregroundStyle(Color.primaryDark)
+                    .fontWeight(.bold)
+                
+                Text("HOME VIEW!!!")
+                    .largeTitleTextStyle()
             }
-            Button("See style guide") {
-                viewModel.seeStyleGuideButtonTapped()
+            
+            HStack {
+                Button("See a Cat!") {
+                    viewModel.seeCatButtonTapped()
+                }
+                .buttonStyle(LightGreenButton())
+                
+                Button("See style guide") {
+                    viewModel.seeStyleGuideButtonTapped()
+                }
+                .buttonStyle(GreenButton(isEnabled: true))
             }
-            .buttonStyle(GreenButton(isEnabled: true))
+            
+            if !viewModel.mockTopMovies.isEmpty {
+                VStack {
+                    HStack {
+                        Text("Top 10 Movies")
+                            .subTitleTextStyle()
+                            .padding()
+                            .border(Color.secondaryDark)
+                        
+                        Spacer()
+                            .frame(maxWidth: .infinity)
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(viewModel.mockTopMovies, id: \.self) { movie in
+                                MovieCard(movie: movie)
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+            
+            if !viewModel.mockRecentMovies.isEmpty {
+                VStack {
+                    HStack {
+                        Text("New Releases")
+                            .subTitleTextStyle()
+                            .padding()
+                            .border(Color.secondaryDark)
+                        
+                        Spacer()
+                            .frame(maxWidth: .infinity)
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(viewModel.mockRecentMovies, id: \.self) { movie in
+                                MovieCard(movie: movie)
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
         }
         .onAppear { self.didAppear?(self) }
         .padding()
