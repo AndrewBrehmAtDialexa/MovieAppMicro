2f70392d48d2babe9f1628097dde0d47bfe48c5f
hain2005 <hain2005@gmail.com>
2024-02-20 08:50:09 -0600

    Add test for singleton UserData

--- UserData/Package.swift
+++ UserData/Package.swift
@@ -14,6 +14,10 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "ApiService"),
+        // TESTING
+        .package(url: "https://github.com/Quick/Quick", from: "7.4.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "13.2.0"),
+        .package(url: "https://github.com/nalexn/ViewInspector", from: "0.9.10"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -23,6 +27,10 @@ let package = Package(
             dependencies: ["ApiService"]),
         .testTarget(
             name: "UserDataTests",
-            dependencies: ["UserData"]),
+            dependencies: ["UserData",
+                .product(name: "Quick", package: "Quick"),
+                .product(name: "Nimble", package: "Nimble"),
+                .product(name: "ViewInspector", package: "ViewInspector"),
+        ]),
     ]
 )
